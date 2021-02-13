6339bcc0b1d22a1d79dd8db12da500d1a595cd6d
Carson Saunders <carsonsaundersalt@gmail.com>
2021-02-13 15:48:50 -0700

    Finished Poker Black Diamond

--- Part2/index.js
+++ Part2/index.js
@@ -6,3 +6,11 @@ function setCard() {
     card.style.color = cardStyle.value;
     console.log(card);
 }
+
+//Black Diamond!
+function resetCards() {
+    document.getElementById('diamonds').setAttribute('style', 'color: grey')
+    document.getElementById('clubs').setAttribute('style', 'color: grey')
+    document.getElementById('hearts').setAttribute('style', 'color: grey')
+    document.getElementById('spades').setAttribute('style', 'color: grey')
+}
